10d5b6bb19eb72daf4eac9ed4163cadb98dd4c60
Arslan-Ahmed-01 <arsoobaloch212@gmail.com>
2026-01-31 13:15:06 +0500

    Update add.js

--- add.js
+++ add.js
@@ -1,17 +1,58 @@
 
-var h = document.getElementById("hour")
-var m = document.getElementById("min")
-var currentdate = document.getElementById("date")
-var currentday = document.getElementById("day")
-var arrayday =["Sunday","Monday","Tueday","Wednesday","Thursday","Friday","Saturday",];
+var h = document.getElementById("hour");
+var m = document.getElementById("min");
+var s = document.getElementById("sec");
+var ampm = document.getElementById("ampm");
+
+var currentDate = document.getElementById("date");
+var currentMonth = document.getElementById("month");
+var currentYear = document.getElementById("year");
+var currentDay = document.getElementById("day");
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
 function clock(){
-    var date = new Date();
-    h.innerText = date.getHours() + ":"
-    m.innerText = date.getMinutes() + ":"  + date.getSeconds()
-    currentdate.innerText = date.getDate()
-    currentday.innerText = arrayday[date.getDay()]
+    var date = new Date();   
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    var session = "AM";
+
+    if(hours >= 12){
+        session = "PM";
+    }
+
+    if(hours > 12){
+        hours = hours - 12;
+    }
+
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    h.innerText = hours + " : ";
+    m.innerText = minutes + " : ";
+    s.innerText = seconds;
+    ampm.innerText = session;
+
+    currentDate.innerText = date.getDate();
+    currentMonth.innerText = months[date.getMonth()];
+    currentYear.innerText = date.getFullYear();
+    currentDay.innerText = days[date.getDay()];
 }
 
-setInterval(clock,1000)
+setInterval(clock, 1000);
+clock();
 
 
